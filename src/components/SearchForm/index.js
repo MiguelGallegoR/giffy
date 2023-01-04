57107c3ac9d1067cfc8e5cf839c0f0a7a699ce82
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-01-04 16:44:56 +0100

    creado SearchForm mejora renderizado

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+export default function SearchForm({ onSubmit }) {
+    const [keyword, setKeyword] = useState('');
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        //navegar a otra ruta
+        onSubmit({keyword})
+        
+        
+    }
+
+    const handleChange = (event) =>{
+        setKeyword(event.target.value);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <button>Buscar</button>
+            <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword}/>
+        </form> 
+    )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,31 +3,23 @@ import { Link, useLocation } from "wouter";
 import ListOfGifs from "../../components/ListOfGifs";
 import { useGifs } from "../../hooks/useGifs";
 import TrendingSearches from "../../components/TrendingSearches";
-
+import SearchForm from "../../components/SearchForm";
 export default function Home(){
-    const [keyword, setKeyword] = useState('');
-    const [path, pushLocation] = useLocation();
     
+    const [path, pushLocation] = useLocation();
     const {loading, gifs} = useGifs()
 
 
-    const handleSubmit = event =>{
-        event.preventDefault();
+    const handleSubmit = ({keyword}) =>{
         //navegar a otra ruta
         pushLocation(`/search/${keyword}`)
         
         
     }
-    const handleChange = (event) =>{
-        setKeyword(event.target.value);
-    }
+    
     return(
         <>
-            <form onSubmit={handleSubmit}>
-                <button>Buscar</button>
-                <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword}/>
-            </form> 
-
+            <SearchForm onSubmit={handleSubmit} />
             <div className="App-main">
                 <div className="App-results">
                     <h3 className="App-title">Última búsqueda</h3>
