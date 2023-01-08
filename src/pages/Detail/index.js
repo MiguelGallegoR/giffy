259c2e5ad3a7f395f1d5e5b39e5b09718244883a
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-01-08 16:55:59 +0100

    actualizada pagina Detail

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Gif from "../../components/Gif";
-import useGlobalGifs from "../../hooks/useGlobalGifs";
+import useSingleGif from "../../hooks/useSingleGif";
+import Spinner from "../../components/Spinner"
+import { Redirect } from "wouter";
+
+
 export default function Detail ({params}){
-    const gifs = useGlobalGifs()
+    const {gif, isLoading, isError} = useSingleGif({id: params.id})
     
-    const gif = gifs.find(singleGif => singleGif.id === params.id)
-    console.log(gif)
-    return <Gif {...gif} />
+    if(isLoading) return <Spinner />
+    if(isError) return <Redirect to='/404' />
+    if(!gif) return null
+
+    return<>
+        <h3 className="App-title">{gif.title}</h3>
+        <Gif {...gif} />
+    </> 
 }
