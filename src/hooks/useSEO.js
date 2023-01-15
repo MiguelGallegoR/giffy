9d9f22e8cf1d348d347deeb9ba5b83410da4c426
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-01-15 18:59:42 +0100

    SEO con react-helmet

---
+++ src/hooks/useSEO.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react"
+
+export default function useSEO({ description, title}){
+    const prevTitle = useRef(document.title)
+    const prevDescription = useRef(document.querySelector('meta[name="description"]').getAttribute('content'))
+    useEffect(()=>{
+        const previousTitle = prevTitle.current
+        if(title){
+            document.title = `${title} | Giffy`
+        }
+
+        return ()=> document.title = previousTitle
+    }, [title])
+
+    useEffect(()=>{
+        const metaDescription = document.querySelector('meta[name="description"]')
+        const previousDescription = prevDescription.current
+        if(description){
+            metaDescription.setAttribute('content', description)
+        }
+        return () => metaDescription.setAttribute('content', previousDescription)
+    }, [description])
+}
